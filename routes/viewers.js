ed8a3f9daa0d59493fcf22d8725bf93c7d4d2688
M. Würbs <m.wuerbs@gmail.com>
2021-05-21 19:49:03 +0200

    introduce fixed rate limit with one song every five minutes

--- routes/viewers.js
+++ routes/viewers.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var rateLimit = require('express-rate-limit');
 
-/* GET users listing. */
+let timeout = 5;
+
+const limiter = rateLimit({
+    windowMs: timeout * 60 * 1000,
+    max: 1,
+    message: `Only one request per ${timeout} minute(s) possible.`
+});
+
 router.get('/', function(req, res) {
     let requestedOrPlayedSongs = globalController.getRequestedSongs().concat(globalController.getPlayedSongs());
 
@@ -16,7 +24,7 @@ router.get('/songs', function (req, res) {
     res.send(globalController.getSongs());
 });
 
-router.post('/songrequest', function (req, res) {
+router.post('/songrequest', limiter, function (req, res) {
     if (!req.body.uniqueId) {
         return res.status(400).send('Playlist or ID missing');
     }
